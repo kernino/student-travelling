a927201b2136acb954d4268924651a5f1c96f108
Pieter Coenen <r0668811@ucll.be>
2019-04-01 15:19:41 +0200

    Begin algemene info

---
+++ app/Http/Controllers/AlgemeneInfoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AlgemeneInfoController extends Illuminate\Routing\Controller
+{
+    private $algemeneInfo;
+    
+    public function __construct() {
+        ;
+    }
+    
+    public function getAlgemeneInfo(){
+        
+    }
+}
