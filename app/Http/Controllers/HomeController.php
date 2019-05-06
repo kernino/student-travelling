3e08332ae8a0522148c4d99faf70d5446811cbd3
daanv <daanv@10.23.142.193>
2019-05-06 15:24:49 +0200

    dummy data Sibert HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Illuminate\Routing\Controller
+{
+    private $aDummy=array(
+    'title' => "USA2019",
+    'start_date' => "10/05/2019",
+    'end_date' => "24/05/2019",
+    'travelcode' => "1234",
+    );
+
+}
+
