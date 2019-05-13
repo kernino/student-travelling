3d6ae4a55c40c9a2bca4c7fc1f91677d49b8cacb
Pieter Coenen <r0668811@ucll.be>
2019-05-13 15:55:28 +0200

    migrations completed

--- database/migrations/2019_05_13_134825_travellers_trips.php
+++ database/migrations/2019_05_13_134825_travellers_trips.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AutosTrips extends Migration
+class TravellersTrips extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class AutosTrips extends Migration
      */
     public function up()
     {
-        Schema::create('autos_trips', function (Blueprint $table) {
-            $table->increments('auto_trip_id')->unique();
+        Schema::create('travellers_trips', function (Blueprint $table) {
+            $table->increments('traveller_trip_id')->unique();
             $table->integer('trip_id');
             $table->foreign('trip_id')->references('trip_id')->on('trips');
-            $table->integer('auto_id');
-            $table->foreign('auto_id')->references('auto_id')->on('autos');
+            $table->integer('traveller_id');
+            $table->foreign('traveller_id')->references('traveller_id')->on('travellers');           
             $table->timestamps();
-        });    
+        });   
     }
 
     /**
@@ -30,6 +30,6 @@ class AutosTrips extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('autos_trips');
+        Schema::dropIfExists('travellers_trips');
     }
 }
