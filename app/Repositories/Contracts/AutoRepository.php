9c458a2ed01f029ae0f3934732de10f7574d73ea
zeno frooninckx <zeno.frooninckx@hotmail.com>
2019-05-25 23:10:42 +0200

    planning blade automatisatie

--- app/Repositories/Contracts/AutoRepository.php
+++ app/Repositories/Contracts/AutoRepository.php
@@ -4,8 +4,6 @@ namespace App\Repositories\Contracts;
 
 interface AutoRepository
 {
-    public function __construct(auto $model);
-
     public function GetAllTravelersByName($sName);
     
     public function GetAllTravelersByAuto($trip);
