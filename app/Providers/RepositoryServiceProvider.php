934445585319679a8b2d4576e28dc3d6b4070c74
zeno frooninckx <zeno.frooninckx@hotmail.com>
2019-05-25 23:18:41 +0200

    planning blade automatisatie

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -14,17 +14,15 @@
     use App\Repositories\Eloquent\EloquentAutoBackend;
     use App\Repositories\Contracts\HotelRepository;
     use App\Repositories\Eloquent\EloquentHotel;
-
-    use App\Repositories\Contracts\PlanningRepository;
-    use App\Repositories\Eloquent\EloquentPlanning;
-
-
+    
     use App\Repositories\Contracts\VervoerRepositoryBackend;
     use App\Repositories\Eloquent\EloquentVervoerBackend;
-
+    
     use App\Repositories\Contracts\AutoRepository;
     use App\Repositories\Eloquent\EloquentAuto;
-
+    
+    use App\Repositories\Contracts\PlanningRepository;
+    use App\Repositories\Eloquent\EloquentPlanning;
 
     class RepositoryServiceProvider extends ServiceProvider
     {
@@ -33,13 +31,9 @@
             $this->app->singleton(InfoRepositoryBackend::class, EloquentInfoBackend::class);
             $this->app->singleton(AutoRepositoryBackend::class, EloquentAutoBackend::class);
             $this->app->singleton(HotelRepository::class, EloquentHotel::class);
-
-            $this->app->singleton(PlanningRepository::class, EloquentPlanning::class);
-
-
+            
             $this->app->singleton(VervoerRepositoryBackend::class, EloquentVervoerBackend::class);
             $this->app->singleton(AutoRepository::class, EloquentAuto::class);
-
+            $this->app->singleton(PlanningRepository::class, EloquentPlanning::class);
         }
     }
-
