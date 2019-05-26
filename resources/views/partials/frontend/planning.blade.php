ac42054295d78307c680418bd12c0611114e06cc
christianSollai <christian.sollai@hotmail.be>
2019-05-26 17:27:05 +0200

    Adding and editing Contecten pagina, Planning, planningDay and inlogScherm

--- resources/views/partials/frontend/planning.blade.php
+++ resources/views/partials/frontend/planning.blade.php
@@ -11,7 +11,7 @@
 @section('content')
 <h1>Planning</h1>
 @foreach ($aPlanning as $location => $dayPlannings)
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 35rem;">
   <div class="card-body">
     <h5 class="card-title">{{$location}}</h5>
     @foreach ($dayPlannings as $day)
