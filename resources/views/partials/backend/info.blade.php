329c24d88ab0349b48882099b856af4bf7e57b6c
Stijn Meerten <r0668329@ucll.be>
2019-05-26 14:21:40 +0200

    view algemene info back-end aangepast

--- resources/views/partials/backend/info.blade.php
+++ resources/views/partials/backend/info.blade.php
@@ -18,24 +18,8 @@
         @include('layouts.error')
     </div>
 </form>
-<hr>
-<form action="{{ Request::url() }}/vlucht" method="POST" class="htmlEditor">
-    <div>
-        {{ csrf_field() }} 
-        <h3>Vlucht info:</h3>
-        <input type="hidden" value="vluchtInfo" name="info_type" />
-        <textarea cols="80" rows="12" id="vlucht_info_content" name="info_content"> 
-        </textarea>
-        
-        <input type="submit" value="Opslaan" name="save"/>
-        <input type="button" value="Annuleren" onclick="history.go(0)"/>
-        
-        @include('layouts.error')
-    </div>
-</form>
 @endsection
 
 @section('page_specific_scripts')
     <script>CKEDITOR.replace( 'algemene_info_content' );</script>
-    <script>CKEDITOR.replace( 'vlucht_info_content' );</script>
 @endsection
